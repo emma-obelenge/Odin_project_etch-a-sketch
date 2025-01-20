3620c04ea4026da9f8e41ae152ae646d154c6b05
emma-obelenge <obeleemmanuel@gmail.com>
2025-01-20 13:24:11 +0100

    project etch a sketch v1

---
+++ script/script.js
@@ -0,0 +1,65 @@
+let divPerSide = 16;
+let totalSquares = divPerSide * divPerSide;
+let userInput;
+
+// creating elements
+const container = document.querySelector(".container");
+const userInputButton = document.createElement("button");
+const gridDiv = document.createElement("div");
+
+// appending initial elements before the gridbox
+container.appendChild(userInputButton);
+container.appendChild(gridDiv);
+
+// initializing attributes
+gridDiv.setAttribute("class", "gridDiv")
+
+// styling
+userInputButton.textContent = "Modify Grid";
+userInputButton.style.marginBottom = "0.5rem";
+
+function userModifiedLoadGrid(userInput) {
+    let userInputValue = parseInt(userInput);
+    if(isNaN(userInputValue) || userInputValue < 1 || userInputValue >100) {
+        alert("Please Enter a valid number (between 1 and 100)");
+        return;
+    } else {
+        divsToRemove = document.querySelectorAll(".squares")
+        divsToRemove.forEach(div => {
+            div.remove();
+        });
+        divPerSide = userInputValue
+        totalSquares = divPerSide * divPerSide;
+        loadGrid();
+    }
+}
+function loadGrid() {
+    gridDiv.style.width = divPerSide + "rem";
+    gridDiv.style.height = divPerSide + "rem";
+    for(i = 1; i <= totalSquares; i++) {
+            const squareDiv = document.createElement("div");
+            squareDiv.setAttribute("id", "square" + i);
+            squareDiv.setAttribute("class", "squares");
+            gridDiv.appendChild(squareDiv);
+    }
+    container.appendChild(gridDiv);
+}
+
+
+// event listeners
+container.addEventListener("mouseover", (eventObj) => {
+    const target = eventObj.target;
+    if(target.classList.contains("squares")) {
+        target.style.backgroundColor = "green";
+    }
+});
+
+userInputButton.addEventListener("click", () => {
+    userInput = prompt("Enter your prefered number of squares per side");
+    userModifiedLoadGrid(userInput);
+
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+    loadGrid();
+})
